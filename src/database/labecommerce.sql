18b348fa6d10d870fd5e4154f10e03ecc4602c49
joaohgs78 <102187505+joaohgs78@users.noreply.github.com>
2023-09-27 21:04:22 -0300

    aprofundamento-sql

--- src/database/labecommerce.sql
+++ src/database/labecommerce.sql
@@ -1,3 +1,4 @@
+-- Active: 1695691257107@@127.0.0.1@3306
 PRAGMA foreign_keys = ON; 
 PRAGMA date_class = 'datetime';
 
@@ -38,3 +39,48 @@ VALUES
     ('p5', 'Produto 5', 99.99, 'Descrição do Produto 5', 'url_imagem_5.jpg');
 
     DROP TABLE products
+
+  --Get All Users--
+  SELECT * FROM users;
+
+  --Get All Products--
+  SELECT * FROM products;
+
+  --Get all Products (funcionalidade 2)--
+
+  SELECT * FROM products WHERE  name='Produto 1'
+
+  --Exercício 2--
+
+  --Create User--
+
+  INSERT INTO users (id, name, email, password)
+  VALUES('004', 'Joao', 'joao@teste.com', '7891027')
+
+--Create Product--
+
+INSERT INTO products (id, name, price, description, image_url)
+VALUES('p6', 'Produto 6', 200.99, 'Descrição do Produto 6', 'url_imagem_6.jpg')
+
+--Delete User by id--
+
+DELETE FROM users WHERE id='004'
+
+--Delete Product by id--
+
+DELETE FROM products WHERE id='p6'
+
+--Edit Product by id--
+
+UPDATE products SET price=999.99 WHERE id='p4'
+
+--  make the query edit all columns of the item --
+
+UPDATE products
+SET 
+  name = 'Produto 7',
+  price = 699.79,
+  description = 'Descrição do Produto 7',
+  image_url = 'novaimagemurl.jpg'
+WHERE id = 'p5';
+
